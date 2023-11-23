1ccaa308692bfcbb4fbaf7699cd68b8f33cffad8
sandakovkirill <129473700+sandakovkirill@users.noreply.github.com>
2023-11-23 20:36:36 +0700

    Add files via upload

---
+++ Stroidom/src/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ремонт на дому</title>
+    <link rel="stylesheet" href="style/normalize.css">
+    <link rel="stylesheet" href="style/style.css">
+    <script src="js/list_dropdown.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,100&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="header_inner">
+                <div class="header_top">
+                    <nav class="menu">
+                        <ul class="communications_list">
+                                <li class="communications_list__item phone-numbers">
+                                    <div class="icon-list"><img src="icons/telephone_icon_menu.png" alt="#"></div>
+                                    <div class="text-list"> <p>Номера для связи</p></div>
+                                    <button id="phoneButton"></button>
+                                </li>
+                                <li class="communications_list__item our-mail">
+                                    <div class="icon-list"><img src="icons/mail.png" alt="#"></div>
+                                    <div class="text-list"> <p>stroidom@gmail.com</p></div>
+                                </li>
+                                <li class="communications_list__item our-telegram"> 
+                                    <div class="icon-list"><img src="icons/telegram.png" alt="#"></div>
+                                    <div class="text-list"> <p>stroidom@gmail.com</p></div>
+                                </li>
+                        </ul>
+                        <ul class="navbar">
+                            <li class="navbar__item"><a href="">Главная</a></li>
+                            <li class="navbar__item"> <img src="icons/line_icon.svg" alt=""></li>
+                            <li class="navbar__item"><a href="#">Наши услуги</a></li>
+                            <li class="navbar__item"> <img src="icons/line_icon.svg" alt=""></li>
+                            <li class="navbar__item"><a href="№">О нас</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header> 
+</body>
+</html>
---
+++ Stroidom/src/js/list_dropdown.js
@@ -0,0 +1,6 @@
+var phoneButton = document.getElementById('phoneButton');
+var phonePopup = document.getElementById('phonePopup');
+
+phoneButton.addEventListener('click', function() {
+  phonePopup.style.display = 'block';
+});
